f2d2b3061a19db0ca5093e49ad82d2cff451895b
Vrinda Bhatia <vrindabhatia999@gmail.com>
2020-09-29 01:44:49 +0530

    commit

--- Files_to_dictioanary/Dict_File.py
+++ Files_to_dictioanary/Dict_File.py
@@ -1,16 +1,61 @@
+
 import csv
 from collections import Counter
 
-file=open("test.txt",'r')
+print("****File_To_Dict****")
+print("1.Read the file as dict")
+print("2.Write to it")
+print("3. view by name")
+print("4.Exit")
+while True:
+ choice=int(input("enter choice:"))
 
-dict={}
+ if choice==1:
 
-for line in file:
-    x=line.split(",")
-    a=x[0]
-    b=x[1]
-    dict[a]=b
+     file = open("Test.txt", 'r')
+     dict = {}
+
+     for line in file:
+         x = line.split(",")
+         a = x[0]
+         b = x[1]
+         # c=len(b)-1
+         # b=b[0:c]
+
+         dict[a] = b
+
+     print(dict)
+
+ elif choice==2:
+     file=open("Test.txt","a")
+
+     write1=input("enter what to append:")
+     file.writelines("\n")
+
+     file.writelines(write1)
+     print("content added")
+
+     file.close()
 
 
-print(dict)
-print(dict['aasha'])
+
+ elif choice==3:
+
+
+     nme=input("hose occupation you want to view?")
+     occu=dict.get(nme,"b")
+     print(occu)
+
+ elif choice==4:
+     break
+
+ else:
+     print("wrong choice")
+
+
+
+
+
+
+
+
