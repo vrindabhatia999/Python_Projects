6f419900f055062e31ecc70c16780dfd3f5daac5
Vrinda Bhatia <vrindabhatia999@gmail.com>
2020-11-05 23:57:36 +0530

    commit

--- Leetcode_questions/Selection sort.py
+++ Leetcode_questions/Selection sort.py
@@ -1,1 +1,16 @@
-def selection(arr)
+def selection(arr):
+    n=len(arr)
+
+    for i in range(n):
+
+        min_pos=i
+        for j in range(i+1,n):
+            if arr[min_pos]>arr[j]:
+                min_pos=j
+
+
+        temp=arr[i]
+        arr[i]=arr[min_pos]
+        arr[min_pos]=temp
+
+arr=[6,7,9,3]
