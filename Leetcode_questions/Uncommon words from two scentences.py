951e00cb2c45611d9331d9081211a1050d8d3664
rvindabhatia99 <vrindabhatia999@gmail.com>
2020-12-05 19:09:33 +0530

    commit

---
+++ Leetcode_questions/Uncommon words from two scentences.py
@@ -0,0 +1,23 @@
+def uncom(s1,s2):
+    a=s1.split()
+    b=s2.split()
+    c=[]
+    d=[]
+
+    for i in a:
+        if i not in b:
+            c.append(i)
+
+
+    for j in b:
+        if j not in a:
+            d.append(j)
+
+
+
+
+    print(c+d)
+
+
+uncom("this apple is sweet","this apple is sour")
+
