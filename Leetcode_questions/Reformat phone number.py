619ef476701cbdf3d82f9d1da94100a1b12582b7
Vrinda Bhatia <vrindabhatia999@gmail.com>
2021-02-04 00:33:20 +0530

    commit

---
+++ Leetcode_questions/Reformat phone number.py
@@ -0,0 +1,9 @@
+def word(s):
+     j="".join(s)
+
+     return j
+
+a=word(["hello","ello","lo"])
+print(a)
+
+    
---
+++ Leetcode_questions/sqrt(x).py
@@ -0,0 +1,6 @@
+import math
+def sqrt(x):
+    return int(math.sqrt(x))
+
+a=sqrt(4)
+print(a)
